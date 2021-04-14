90e3b82221fb39520271bea961b959cf3d9f3d3d
Patrick <patrickwhalen2016@gmail.com>
2021-04-14 02:19:01 -0400

    Started renderTodos function and button

--- index/app.js
+++ index/app.js
@@ -1,1 +1,9 @@
-let todos = [];
+let todos = ['pp'];
+
+const renderTodos = function(){
+  console.log(todos);
+};
+
+document.getElementById('button').addEventListener('click', function(){
+  console.log(renderTodos());
+});
