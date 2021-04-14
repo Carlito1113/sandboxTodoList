0da4a0943f7db27ed7b45e93a23e1b0f3679e79e
Carlito Algarin <carlito1113@yahoo.com>
2021-04-14 02:46:25 -0400

    added a function to create and append todo elements into the DOM

--- index/app.js
+++ index/app.js
@@ -1,9 +1,33 @@
-let todos = ['pp'];
+let todos = [];
 
-const renderTodos = function(){
-  console.log(todos);
-};
+const filters = {
+  searchText: ''
+}
+
+// const renderTodos = function(todos, filters){
+//   todos.forEach(todo, function () {
+//     const filteredTodos = todo.filter(todo)
+
+//   })
+// };
+
+const generateTodoDOM = function () {
+  const div = document.createElement('div')
+  const span = document.createElement('span')
+  const checkbox = document.createElement('input')
+  const deleteButton = document.createElement('button')
+
+  checkbox.type = 'checkbox'
+  span.textContent = 'Unnamed todo'
+  deleteButton.textContent = 'x'
+  div.appendChild(checkbox)
+  div.appendChild(span)
+  div.appendChild(deleteButton)
+  document.body.appendChild(div)
+
+  return div
+}
 
 document.getElementById('button').addEventListener('click', function(){
-  console.log(renderTodos());
+  generateTodoDOM()
 });
