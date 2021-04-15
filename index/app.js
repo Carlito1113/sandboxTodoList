9b047fd5cb91c6b7d0a3fdd493655a1c5da422dc
Carlito Algarin <carlito1113@yahoo.com>
2021-04-15 17:34:35 -0400

    created a new file for the functions to increase code legibility, removed old method of setting an id and replaced it with a function call to uuidv4 to create a random id for each todo. Cleaned up some code that was no longer needed.

--- index/app.js
+++ index/app.js
@@ -1,58 +1,8 @@
-let todos = [];
+const todos = [];
 
 const filters = {
-  searchText: ''
-}
-
-// To keep track of each div created for each todo.
-let divIndex = 0;
-
-// const renderTodos = function(todos, filters){
-//   todos.forEach(todo, function () {
-//     const filteredTodos = todo.filter(todo)
-
-//   })
-// };
-
-const generateTodoDOM = function () {
-  const div = document.createElement('div');
-  const span = document.createElement('span')
-  const checkbox = document.createElement('input')
-  const deleteButton = document.createElement('button')
-  const input = document.getElementById('input')
-
-  checkbox.type = 'checkbox'
-
-
-  // Creates new div and adds an index to the div being created.
-  if (input.value !== '') {
-    span.textContent = input.value;
-    todos.push(span.textContent);
-    div.setAttribute('id', divIndex);
-    divIndex++;
-    console.log(todos);
-    //console.log(div.getAttribute('id'));
-  } else {
-    span.textContent = 'Unnamed todo'
-  }
-
-  deleteButton.textContent = 'x'
-  div.appendChild(checkbox);
-  div.appendChild(span);
-  div.appendChild(deleteButton);
-  document.body.appendChild(div);
-
-  // Gets rid of the appropriate todo when 'x' clicked and removes it from the array
-  deleteButton.onclick = function(){
-    //console.log(div.getAttribute('id'));
-    div.removeChild(checkbox);
-    div.removeChild(span);
-    div.removeChild(deleteButton);
-    todos.splice(div.getAttribute('id'), 1, '');
-    console.log(todos);
-  }
-
-  return div;
+  searchText: '',
+  hideCompleted: false
 }
 
 document.getElementById('button').addEventListener('click', function(){
