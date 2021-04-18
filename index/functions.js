0bfdebe720652adf4b9ea46ce1eceecfdcdb5e62
Carlito <carlito1113@yahoo.com>
2021-04-18 18:44:58 -0400

    fixed an issue where the todo divs, when deleted, would only remove their inner elements and the empty todo div would be left behind leaving gaps within the todo list. Also added in some light css styling

--- index/functions.js
+++ index/functions.js
@@ -1,6 +1,6 @@
 // Individual Todo DOM creation
 const generateTodoDOM = function () {
-  const div = document.createElement('div');
+  const todo = document.createElement('div');
   const span = document.createElement('span')
   const checkbox = document.createElement('input')
   const deleteButton = document.createElement('button')
@@ -8,33 +8,31 @@ const generateTodoDOM = function () {
 
   checkbox.type = 'checkbox'
 
-
   // Creates new div and adds an index to the div being created.
   if (input.value !== '') {
     span.textContent = input.value;
     todos.push(span.textContent);
-    div.setAttribute('id', uuidv4());
+    todo.setAttribute('id', uuidv4());
     console.log(todos);
-    console.log(div.getAttribute('id'));
+    console.log(todo.getAttribute('id'));
   } else {
     span.textContent = 'Unnamed todo'
   }
 
   deleteButton.textContent = 'x'
-  div.appendChild(checkbox);
-  div.appendChild(span);
-  div.appendChild(deleteButton);
-  document.body.appendChild(div);
+  todo.className = 'todo'
+  span.className = 'span'
+  todo.appendChild(checkbox);
+  todo.appendChild(span);
+  todo.appendChild(deleteButton);
+  document.body.appendChild(todo);
 
   // Gets rid of the appropriate todo when 'x' clicked and removes it from the array
   deleteButton.onclick = function(){
-    //console.log(div.getAttribute('id'));
-    div.removeChild(checkbox);
-    div.removeChild(span);
-    div.removeChild(deleteButton);
-    todos.splice(div.getAttribute('id'), 1);
+    todo.remove()
+    todos.splice(todo.getAttribute('id'), 1);
     console.log(todos);
   }
 
-  return div;
+  return todo;
 }
